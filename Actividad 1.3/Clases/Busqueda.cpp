4add322dc0311f90f6ce9b4e3553c1e2ffd5d764
Gabriel Edid <66497467+GabrielEdid@users.noreply.github.com>
2023-10-25 09:45:05 -0600

    Delete Actividad 1.3 directory

--- Actividad 1.3/Clases/Busqueda.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Busqueda.cpp
-//  busqueda_secuencial
-//
-//  Created by Vicente Cubells Nonell on 10/09/20.
-//  Copyright © 2020 Vicente Cubells Nonell. All rights reserved.
-//  Modificado por Gabriel Edid Harari A01782146 y Paolo Antonio Pires Cano A01749355
-
-#include "Busqueda.hpp"
